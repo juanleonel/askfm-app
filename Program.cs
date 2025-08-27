9b3eb4d1adaa72d9eba01f9c5391c8bc7aec7de2
Juan L. Albores <juanl.albores.c@gmail.com>
2025-08-26 23:10:09 -0500

    Added config to refresh the views

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
